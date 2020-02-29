c2bbf096146f08061fb4902dbde2523f0c12f7d2
Wouter Wijsman <wwijsman@live.nl>
2020-02-29 14:29:54 +0100

    Excluded J as well, since it doesn't look like an uppercase letter This is an issue with the font used.

--- steam_buddy/authenticator.py
+++ steam_buddy/authenticator.py
@@ -7,10 +7,11 @@ import psutil
 
 def generate_password(length: int) -> str:
     alphabet = string.ascii_uppercase + string.digits
-    # Exclude the O, I and 0
+    # Exclude the O, I, J and 0
     alphabet = alphabet.replace("O", "")
     alphabet = alphabet.replace("I", "")
     alphabet = alphabet.replace("0", "")
+    alphabet = alphabet.replace("J", "")  # Looks too much like a lowercase letter
     password = ''.join(secrets.choice(alphabet) for i in range(length))
     return password
 
