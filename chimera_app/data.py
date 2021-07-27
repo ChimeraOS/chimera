0ccfc576e01ead4dc4b773390849af8be0650736
Joaquín Ignacio Aramendía <samsagax@gmail.com>
2021-07-27 00:38:06 -0300

    Add data download handler

---
+++ chimera_app/data.py
@@ -0,0 +1,129 @@
+"""Data handling for Chiemra app"""
+import os
+import shutil
+import json
+import zipfile
+import requests
+from chimera_app.utils import file_exists
+
+
+class Downloader():
+    """Downlaoder for data files from our repository
+    Usage:
+        dl = Downloader()
+        dl.update()
+
+    This will check for updates of the data repository and download it
+
+    Update can be forced by giving the `force` flag:
+        dl.update(force=True)
+    """
+
+    db_path: str
+    channel: str
+
+    def __init__(self,
+                 channel='master',
+                 db_path='~/.local/share/chimera/data'):
+        """Setup this data doenloader with a database directory path"""
+        self.channel = channel
+        self.db_path = os.path.expanduser(db_path)
+
+    def fetch_latest(self) -> None:
+        api_url = 'https://api.github.com/repos/chimeraos/chimera-data/branches'
+        resp = requests.get(api_url)
+        db_path = os.path.join(self.db_path, "branches.json")
+        with open(db_path, 'w') as db_file:
+            db_file.write(json.dumps(resp.json(), sort_keys=True, indent=4))
+
+    def get_installed(self) -> dict:
+        version_path = os.path.join(self.db_path, "versions.json")
+        version = {}
+        if file_exists(version_path):
+            with open(version_path) as version_file:
+                version = json.load(version_file)
+        return version
+
+    def get_installed_version(self) -> str:
+        versions = self.get_installed()
+        if versions['installed']['name'] == self.channel:
+            ver = versions['installed']['sha']
+        else:
+            ver = None
+        return ver
+
+    def get_available_versions(self) -> list:
+        branches_path = os.path.join(self.db_path, "branches.json")
+        versions = []
+        if file_exists(branches_path):
+            with open(branches_path) as branches_file:
+                branches = json.load(branches_file)
+                for branch in branches:
+                    versions.append({
+                        "name": branch['name'],
+                        "sha": branch['commit']['sha']
+                                    })
+        return versions
+
+    def check_update(self) -> bool:
+        available_versions = self.get_available_versions()
+        installed_version = self.get_installed_version()
+        updated = False
+        for versions in available_versions:
+            if self.channel == versions['name']:
+                if (installed_version == versions['sha']):
+                    updated = True
+        return updated
+
+    def get_update_sha(self) -> str:
+        available_versions = self.get_available_versions()
+        sha = None
+        for versions in available_versions:
+            if self.channel == versions['name']:
+                sha = versions['sha']
+        return sha
+
+    def download_package(self, sha):
+        url = ('https://github.com/ChimeraOS/chimera-data/archive/' +
+               sha + '.zip')
+        resp = requests.get(url, timeout=1)
+        if resp.status_code == 200:
+            zip_path = os.path.join(self.db_path, "data.zip")
+            with open(zip_path, 'wb') as zip_file:
+                zip_file.write(resp.content)
+        else:
+            raise Exception(f"File not found for sha: {sha}")
+
+    def update(self, force=False) -> bool:
+        updated = self.check_update()
+        if not updated or force:
+            sha = self.get_update_sha()
+            self.download_package(sha)
+            zip_path = os.path.join(self.db_path, "data.zip")
+            version_path = os.path.join(self.db_path, "versions.json")
+            with zipfile.ZipFile(zip_path, 'r') as zip_file:
+                zip_file.extractall(self.db_path)
+            dirname = os.path.join(self.db_path, 'chimera-data-' + sha)
+            for data in os.listdir(dirname):
+                src_path = os.path.join(dirname, data)
+                dest_path = os.path.join(self.db_path, data)
+                if os.path.isdir(src_path):
+                    shutil.copytree(src_path,
+                                    dest_path,
+                                    dirs_exist_ok=True)
+                elif os.path.isfile(src_path):
+                    shutil.copyfile(src_path, dest_path)
+            shutil.rmtree(dirname)
+            with open(version_path, 'w') as version_file:
+                ver_content = {"installed":
+                               {
+                                   "name": self.channel,
+                                   "sha": sha
+                                }
+                               }
+                version_file.write(json.dumps(ver_content,
+                                              sort_keys=True,
+                                              indent=4)
+                                   )
+            updated = True
+        return updated
---
+++ tests/test_downloader.py
@@ -0,0 +1,101 @@
+import os
+import json
+import pytest
+from chimera_app.data import Downloader
+
+
+@pytest.fixture
+def empty_data(fs):
+    files_path = os.path.join(os.path.dirname(__file__), 'files')
+    fs.create_dir(os.path.expanduser('~/.local/share/chimera/data'))
+    fs.add_real_file(os.path.join(files_path, 'branches.json'))
+    yield fs
+
+
+@pytest.fixture
+def updatable_fs(empty_data):
+    files_path = os.path.join(os.path.dirname(__file__), 'files')
+    empty_data.add_real_file(os.path.join(files_path, 'versions.json'),
+                             target_path=os.path.expanduser(
+                                '~/.local/share/chimera/data/versions.json'),
+                             read_only=False,
+                             )
+    empty_data.add_real_file(os.path.join(files_path, 'branches.json'),
+                             target_path=os.path.expanduser(
+                                '~/.local/share/chimera/data/branches.json'),
+                             read_only=False
+                             )
+
+    yield empty_data
+
+
+@pytest.fixture
+def branches_content():
+    files_path = os.path.join(os.path.dirname(__file__), 'files')
+    branches_mock = os.path.join(files_path, "branches.json")
+    yield open(branches_mock).read()
+
+
+@pytest.fixture
+def zip_content():
+    files_path = os.path.join(os.path.dirname(__file__), 'files')
+    zip_mock = os.path.join(files_path, "test.zip")
+    yield open(zip_mock, 'rb').read()
+
+
+@pytest.fixture
+def branches_mock(requests_mock,
+                  branches_content):
+    api_url = 'https://api.github.com/repos/chimeraos/chimera-data/branches'
+    requests_mock.get(api_url,
+                      json=branches_content)
+    yield requests_mock
+
+
+@pytest.fixture
+def data_mock(requests_mock,
+              zip_content):
+    api_url = ('https://github.com/chimeraos/chimera-data/archive/'
+               '6fd2a1af5ae6cd0acef222908374fe6ba8164083.zip')
+    requests_mock.get(api_url,
+                      content=zip_content)
+    yield requests_mock
+
+
+def test_fetch_latest(branches_mock,
+                      branches_content,
+                      empty_data):
+    branches_file = os.path.expanduser(
+        '~/.local/share/chimera/data/branches.json')
+
+    downloader = Downloader()
+    downloader.fetch_latest()
+    assert(os.path.exists(branches_file))
+    with open(branches_file) as file:
+        branches = json.load(file)
+        assert(branches == branches_content)
+
+
+def test_download_updated(updatable_fs):
+    downloader = Downloader()
+    assert(not downloader.check_update())
+
+
+def test_download_package(data_mock,
+                          updatable_fs):
+    data_path = os.path.expanduser('~/.local/share/chimera/data/data.zip')
+
+    downloader = Downloader()
+    downloader.download_package('6fd2a1af5ae6cd0acef222908374fe6ba8164083')
+    assert(os.path.exists(data_path))
+
+
+def test_download_update(branches_mock,
+                         data_mock,
+                         updatable_fs):
+    downloader = Downloader()
+    assert(not downloader.check_update())
+    assert(downloader.update())
+
+    assert(downloader.get_installed_version() ==
+           '6fd2a1af5ae6cd0acef222908374fe6ba8164083')
