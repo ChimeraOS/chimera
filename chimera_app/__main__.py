ChimeraOS/chimera

commit ec9a50d602f72aa0d6006a62975daeaf5355e496

Download data from repository (#191)
* Add data download handler

* Refactor Context and functions to new structure

- Add a `context` module to handle context in a simpler way (ditch the
singleton)
- Remove all static references since we should not ship those anymore
- Config should not download tweaks anymore, it's downloader's
  resposibility now
- Fuse `functions` into `utils`
- Remove functions in `utils` that wrap builtins (os.path related mostly)
- Rewrite tests for the new structure

* Add update options for main executable

* Remove downloadable data

* Change steam-patch working directory

- Patch now should work inside the data directory or exit if it does not
  exist

Note this change need to review all `src` files to the new structure
inside tweaks file

* Call --update along with --tweaks

* Don't fail if there is no userdata on steam

* Revert steam-tweaks.yaml removal for now

* Fix logic error in GOG game retrieval

* Improve readability on shortcuts

chimera_app/__main__.py
@@ -2,6 +2,7 @@ import os
 import shutil
 import argparse
 import bottle
+from chimera_app.data import update_data
 from chimera_app.server import server
 from chimera_app.compat_tools import install_all_compat_tools
 from chimera_app.shortcuts import create_all_shortcuts
@@ -13,10 +14,19 @@ def setup_argparse():
     parser = argparse.ArgumentParser(
         description='Chimera app for managing ChimeraOS')
 
+    parser.add_argument('-u', '--update',
+                        action="store-true",
+                        help='Update data from repository'
+                        )
+    parser.add_argument('-f', '--force-update',
+                        action="store-true",
+                        help='Force update even if already updated'
+                        )
     group_ex = parser.add_mutually_exclusive_group()
     group_ex.add_argument('-d', '--daemon',
                           action="store_true",
-                          help='Start chimera app web server (default behaviour)'
+                          help=('Start chimera app web server '
+                                '(default behaviour)')
                           )
     group_ex.add_argument('-t', '--tweaks',
                           action="store_true",
@@ -32,7 +42,7 @@ def setup_argparse():
                           )
     group_ex.add_argument('-g', '--config',
                           action="store_true",
-                          help='Download tweak file and apply configuration for Steam games'
+                          help=('Apply configuration for Steam games')
                           )
 
     return parser.parse_args()
@@ -53,8 +63,15 @@ def run_server():
 def main():
     args = setup_argparse()
     if (args.daemon or
-    not (args.daemon or args.compat or args.shortcuts or args.config or args.tweaks)):
+            not ((args.daemon
+                  or args.compat
+                  or args.shortcuts
+                  or args.config
+                  or args.tweaks
+                  or args.update))):
         run_server()
+    if args.update:
+        update_data(args.force_update)
     if args.compat:
         install_all_compat_tools()
     if args.shortcuts:

chimera_app/config.py
@@ -1,6 +1,6 @@
 import os
-from chimera_app.utils import ChimeraContext
 from chimera_app.settings import Settings
+import chimera_app.context as context
 from chimera_app.ftp.server import Server as FTPServer
 from chimera_app.authenticator import Authenticator, generate_password
 from chimera_app.ssh_keys import SSHKeys
@@ -8,8 +8,6 @@ from chimera_app.steamgrid.steamgrid import Steamgrid
 from chimera_app.streaming import StreamServer
 from chimera_app.mangohud_config import MangoHudConfig
 
-
-se = ChimeraContext()
 
 RESOURCE_DIR = os.getcwd()
 if not os.path.isfile(os.path.join(RESOURCE_DIR, 'views/base.tpl')):
@@ -19,13 +17,13 @@ AUTHENTICATOR_PATH = os.path.abspath('bin/chimera-authenticator')
 if not os.path.isfile(AUTHENTICATOR_PATH):
     AUTHENTICATOR_PATH = "/usr/share/chimera/bin/chimera-authenticator"
 
-SHORTCUT_DIR = se.SHORTCUT_DIRS[0]
-BANNER_DIR = se.DATA_HOME + '/chimera/banners'
-CONTENT_DIR = se.DATA_HOME + '/chimera/content'
-RECORDINGS_DIR = se.DATA_HOME + '/chimera/recordings'
-SETTINGS_DIR = se.CONFIG_HOME + '/chimera'
-UPLOADS_DIR = os.path.join(se.CACHE_HOME, 'chimera', 'uploads')
-MANGOHUD_DIR = se.CONFIG_HOME + "/MangoHud"
+SHORTCUT_DIR = context.SHORTCUT_DIRS[0]
+BANNER_DIR = context.DATA_HOME + '/chimera/banners'
+CONTENT_DIR = context.DATA_HOME + '/chimera/content'
+RECORDINGS_DIR = context.DATA_HOME + '/chimera/recordings'
+SETTINGS_DIR = context.CONFIG_HOME + '/chimera'
+UPLOADS_DIR = os.path.join(context.CACHE_HOME, 'chimera', 'uploads')
+MANGOHUD_DIR = context.CONFIG_HOME + "/MangoHud"
 
 PLATFORMS = {
     "32x":         "32X",

chimera_app/utils.py
@@ -1,6 +1,11 @@
 """Utilities for the chimera_app tools"""
 import os
+import re
+import shutil
 from datetime import datetime
+import yaml
+from PIL import Image, ImageFont, ImageDraw
+import chimera_app.context as context
 
 
 def ensure_directory(directory):
@@ -11,14 +16,6 @@ def ensure_directory(directory):
 def ensure_directory_for_file(file):
     d = os.path.dirname(file)
     ensure_directory(d)
-
-
-def file_exists(file):
-    return os.path.exists(file)
-
-
-def directory_exists(directory):
-    return os.path.isdir(directory)
 
 
 def yearsago(years):
@@ -35,97 +32,132 @@ def replace_all(text, dic):
     return text
 
 
-class ChimeraContext:
-    """Singleton class to manage the Steam environment.
-    It contains variables and common functions to be used in all
-    chimera tools"""
+def sanitize(string):
+    if isinstance(string, str):
+        retval = string
+        for r in ['\n', '\r', '/', '\\', '\0']:
+            retval = retval.replace(r, '_')
+        retval.replace('"', '')
+        return retval
+    return string
 
-    _instance = None
 
-    def __new__(cls):
-        if cls._instance is None:
-            cls._instance = super(ChimeraContext, cls).__new__(cls)
-            cls._instance.__init()
-        return cls._instance
+def load_shortcuts(platform):
+    shortcuts = []
+    ensure_directory(context.SHORTCUT_DIRS[0])
 
-    def __init(self):
-        """Initialize all directories and common file names"""
-        self.STATIC_DATA = self.__get_static_data_dir()
-        self.CACHE_HOME = self.__get_cache_home_dir()
-        self.CONFIG_HOME = self.__get_config_home_dir()
-        self.DATA_HOME = self.__get_data_home_dir()
-        self.COMPAT_DATA_FILE = self.__get_compat_data_file()
-        self.SHORTCUT_DIRS = self.__get_shortcut_dirs()
-        self.STATIC_COMPAT_TOOLS = self.__get_static_compat_tools_dir()
-        self.STEAM_DIR = self.__get_steam_dir()
-        self.STEAM_APPS_DIR = self.__get_steam_apps_dir()
-        self.STEAM_USER_DIRS = self.__get_steam_user_dirs()
-        self.STEAM_COMPAT_TOOLS = self.__get_steam_compat_tools_dir()
-        self.STEAM_CONFIG_FILE = self.__get_steam_config_file()
-        self.STATIC_TWEAKS_FILE = self.__get_static_tweaks_file()
-        self.MAIN_TWEAKS_FILE = self.__get_main_tweaks_file()
-        self.LOCAL_TWEAKS_FILE = self.__get_local_tweaks_file()
-        self.TIME_WARP = os.environ.get('TIME_WARP')
+    shortcuts_file = (context.SHORTCUT_DIRS[0] +
+                      "/chimera.{platform}.yaml".format(platform=platform))
+    if os.path.exists(shortcuts_file):
+        shortcuts = yaml.load(open(shortcuts_file), Loader=yaml.Loader)
 
-    def __get_static_data_dir(self):
-        return '/usr/share/chimera'
+    if not shortcuts:
+        shortcuts = []
 
-    def __get_cache_home_dir(self):
-        if 'XDG_CACHE_HOME' in os.environ:
-            cache = os.environ['XDG_CACHE_HOME']
-        else:
-            cache = os.environ['HOME'] + '/.cache'
-        return cache
+    return shortcuts
 
-    def __get_config_home_dir(self):
-        if 'XDG_CONFIG_HOME' in os.environ:
-            config = os.environ['XDG_CONFIG_HOME']
-        else:
-            config = os.environ['HOME'] + '/.config'
-        return config
 
-    def __get_data_home_dir(self):
-        if 'XDG_DATA_HOME' in os.environ:
-            data_home = os.environ['XDG_DATA_HOME']
-        else:
-            data_home = os.environ['HOME'] + '/.local/share'
-        return data_home
+def delete_file_link(base_dir, platform, name):
+    e = re.escape(name) + r"\.[^.]+$"
+    d = os.path.join(base_dir, platform)
+    links = []
+    if os.path.isdir(d):
+        links = [os.path.join(d, l) for l in os.listdir(d) if re.match(e, l)]
 
-    def __get_compat_data_file(self):
-        return self.CACHE_HOME + '/steam-shortcuts-compat.yaml'
+    if len(links) < 1:
+        return
 
-    def __get_shortcut_dirs(self):
-        return self.DATA_HOME + '/chimera/shortcuts'
+    for link in links:
+        if os.path.islink(link) or os.path.exists(link):
+            os.remove(link)
 
-    def __get_static_compat_tools_dir(self):
-        return '/usr/share/chimera/compat-tools'
 
-    def __get_steam_dir(self):
-        return self.DATA_HOME + '/Steam'
+def is_direct(platform, content_type):
+    return ((platform == "arcade" or platform == "neo-geo") and
+            content_type == "content")
 
-    def __get_steam_apps_dir(self):
-        return self.STEAM_DIR + '/steamapps/common'
 
-    def __get_steam_user_dirs(self):
-        base = os.path.join(self.STEAM_DIR, 'userdata')
-        ensure_directory(base)
-        user_dirs = []
-        for d in os.listdir(base):
-            if d not in ['anonymous', 'ac', '0']:
-                user_dirs.append(os.path.join(base, d))
-        return user_dirs
+def upsert_file(src_path, base_dir, platform, name, dst_name):
+    if not src_path:
+        return
 
-    def __get_steam_compat_tools_dir(self):
-        return self.STEAM_DIR + '/compatibilitytools.d'
+    content_type = os.path.basename(base_dir)
+    filename = sanitize(dst_name)
+    file_dir = f"{base_dir}/{platform}/.{name}"
 
-    def __get_steam_config_file(self):
-        return self.STEAM_DIR + '/config/config.vdf'
+    # mame ROM files have dependencies on each other,
+    # so store them all in a single directory
+    if is_direct(platform, content_type):
+        file_dir = f"{base_dir}/{platform}/.{platform}"
 
-    def __get_main_tweaks_file(self):
-        return self.DATA_HOME + '/chimera/steam-tweaks.yaml'
+    if not os.path.exists(file_dir):
+        os.makedirs(file_dir)
 
-    def __get_local_tweaks_file(self):
-        return self.CONFIG_HOME + '/steam-tweaks.yaml'
+    file_path = f"{file_dir}/{filename}"
+    if os.path.exists(file_path):
+        os.remove(file_path)
 
-    def __get_static_tweaks_file(self):
-        return '/usr/share/chimera/steam-tweaks.yaml'
+    shutil.move(src_path, file_path)
+
+    _, ext = os.path.splitext(filename)
+    dst = f"{base_dir}/{platform}/{name}{ext}"
+
+    delete_file_link(base_dir, platform, name)
+    os.symlink(file_path, dst)
+
+    # mame requires ROM files to have a specific name,
+    # so launch original file directly
+    if is_direct(platform, content_type):
+        return file_path
+
+    return dst
+
+
+def strip(string):
+    if string.startswith('"') and string.endswith('"'):
+        return string[1:-1]
+    return string
+
+
+def delete_file(base_dir, platform, name):
+    if is_direct(platform, os.path.basename(base_dir)):
+        shortcuts = load_shortcuts(platform)
+        matches = ([e for e in shortcuts if e['name'] == name
+                    and e['cmd'] == platform])
+        shortcut = matches[0]
+        if 'dir' in shortcut and 'params' in shortcut:
+            file_path = os.path.join(strip(shortcut['dir']),
+                                     strip(shortcut['params']))
+            if os.path.exists(file_path):
+                os.remove(file_path)
+    else:
+        file_dir = f"{base_dir}/{platform}/.{name}"
+        if os.path.exists(file_dir):
+            shutil.rmtree(file_dir)
+
+    delete_file_link(base_dir, platform, name)
+
+
+def generate_banner(text, path):
+    # The thumbnail size used by Steam is set
+    banner_width = 460
+    banner_height = 215
+    banner = Image.new('RGB', (banner_width, banner_height), color=(0, 0, 0))
+
+    font = ImageFont.truetype("/usr/share/fonts/TTF/DejaVuSansMono-Bold.ttf",
+                              24)
+
+    text_width, text_height = font.getsize(text)
+
+    # Shorten the text if it doesn't fit on the image
+    while text_width > banner_width:
+        text = text[:-4] + "..."
+        text_width, text_height = font.getsize(text)
+
+    text_x = int(banner_width / 2 - text_width / 2)
+    text_y = int(banner_height / 2 - text_height / 2)
+
+    title = ImageDraw.Draw(banner)
+    title.text((text_x, text_y), text, font=font, fill=(255, 255, 255))
+
+    banner.save(path)

chimera_app/compat_tools.py
@@ -2,23 +2,31 @@
 
 import os
 import shutil
-from chimera_app.utils import ChimeraContext
+import chimera_app.context as context
 from chimera_app.utils import ensure_directory
-from chimera_app.utils import directory_exists
 from chimera_app.utils import replace_all
 
 
 def install_all_compat_tools():
-    cc = ChimeraContext()
-    ensure_directory(cc.STEAM_COMPAT_TOOLS)
-    for tool_dir in os.listdir(cc.STATIC_COMPAT_TOOLS):
-        steam_tool = os.path.join(cc.STEAM_COMPAT_TOOLS, tool_dir)
-        static_tool = os.path.join(cc.STATIC_COMPAT_TOOLS, tool_dir)
-        if not directory_exists(steam_tool):
-            shutil.copytree(static_tool,
+    tools_dir = context.TOOLS_DIR
+    stub_file = os.path.join(tools_dir, 'tool-stub.tpl')
+    if (not os.path.isdir(tools_dir)
+            or (not os.path.isfile(stub_file))):
+        print(f'No tools to install from {tools_dir} or missing stub template')
+        return
+
+    ensure_directory(context.STEAM_COMPAT_TOOLS)
+    for tool_dir in os.listdir(tools_dir):
+        steam_tool = os.path.join(context.STEAM_COMPAT_TOOLS, tool_dir)
+        tool = os.path.join(context.TOOLS_DIR, tool_dir)
+        if not os.path.isdir(tool):
+            continue
+        if not os.path.isdir(steam_tool):
+            shutil.copytree(tool,
                             steam_tool)
-            si = CompatToolStubInfo.load_stub_info(os.path.join(static_tool, 'stub.info'))
-            ct = CompatTool(si)
+            si = CompatToolStubInfo.load_stub_info(
+                os.path.join(tool, 'stub.info'))
+            ct = CompatTool(stub_file, si)
             ct.write_stub(steam_tool)
 
 
@@ -51,25 +59,21 @@ class CompatToolStubInfo():
 class CompatTool():
     """Class to manage compatibility tools"""
 
-    _context: ChimeraContext
     _template: str
     stub: CompatToolStubInfo
 
-    def __init__(self, stub_info):
-        self._context = ChimeraContext()
-        self._template = open(os.path.join(self._context.STATIC_DATA,
-                              'steam-compat-tool-stub.tpl'),
-                              'r').read()
+    def __init__(self, template_path, stub_info):
+        self._template = open(template_path, 'r').read()
         self.stub = stub_info
 
     def write_stub(self,
-                   path):
+                   tool_path):
         replacements = {
             '%TOOL_URL%': self.stub.url,
             '%TOOL_MD5SUM%': self.stub.md5sum,
             '%TOOL_CMD%': self.stub.cmd
         }
-        stub_path = os.path.join(path, self.stub.cmd)
+        stub_path = os.path.join(tool_path, self.stub.cmd)
         with open(stub_path, 'w') as stub_file:
             stub_file.write(replace_all(self._template, replacements))
         os.chmod(stub_path, 0o775)

chimera_app/context.py
@@ -1,0 +1,30 @@
+"""Context variables for all Chimera tools"""
+
+import os
+
+
+def __get_steam_user_dirs(steam_dir):
+    base = os.path.join(steam_dir, 'userdata')
+    user_dirs = []
+    if os.path.isdir(base):
+        for d in os.listdir(base):
+            if d not in ['anonymous', 'ac', '0']:
+                user_dirs.append(os.path.join(base, d))
+    return user_dirs
+
+
+CACHE_HOME = os.path.expanduser('~/.cache')
+CONFIG_HOME = os.path.expanduser('~/.config')
+DATA_HOME = os.path.expanduser('~/.local/share')
+COMPAT_DATA_FILE = os.path.join(CACHE_HOME, 'steam-shortcuts-compat.yaml')
+SHORTCUT_DIRS = os.path.join(DATA_HOME, 'chimera/shortcuts')
+MAIN_TWEAKS_FILE = os.path.join(DATA_HOME, 'chimera/data/tweaks/steam.yaml')
+TOOLS_DIR = os.path.join(DATA_HOME, 'chimera/data/compat/tools')
+PATCH_DIR = os.path.join(DATA_HOME, 'chimera/data/patch')
+LOCAL_TWEAKS_FILE = os.path.join(CONFIG_HOME, 'steam-tweaks.yaml')
+TIME_WARP = os.environ.get('TIME_WARP')
+STEAM_DIR = os.path.join(DATA_HOME, 'Steam')
+STEAM_APPS_DIR = os.path.join(STEAM_DIR, 'steamapps/common')
+STEAM_USER_DIRS = __get_steam_user_dirs(STEAM_DIR)
+STEAM_COMPAT_TOOLS = os.path.join(STEAM_DIR, 'compatibilitytools.d')
+STEAM_CONFIG_FILE = os.path.join(STEAM_DIR, 'config/config.vdf')

chimera_app/data.py
@@ -1,0 +1,134 @@
+"""Data handling for Chiemra app"""
+import os
+import shutil
+import json
+import zipfile
+import requests
+
+
+def update_data(force=False) -> bool:
+    dl = Downloader()
+    return dl.update(force=force)
+
+
+class Downloader():
+    """Downlaoder for data files from our repository
+    Usage:
+        dl = Downloader()
+        dl.update()
+
+    This will check for updates of the data repository and download it
+
+    Update can be forced by giving the `force` flag:
+        dl.update(force=True)
+    """
+
+    db_path: str
+    channel: str
+
+    def __init__(self,
+                 channel='master',
+                 db_path='~/.local/share/chimera/data'):
+        """Setup this data doenloader with a database directory path"""
+        self.channel = channel
+        self.db_path = os.path.expanduser(db_path)
+
+    def fetch_latest(self) -> None:
+        api_url = ('https://api.github.com/repos/chimeraos/'
+                   'chimera-data/branches')
+        resp = requests.get(api_url)
+        db_path = os.path.join(self.db_path, "branches.json")
+        with open(db_path, 'w') as db_file:
+            db_file.write(json.dumps(resp.json(), sort_keys=True, indent=4))
+
+    def get_installed(self) -> dict:
+        version_path = os.path.join(self.db_path, "versions.json")
+        version = {}
+        if os.path.exists(version_path):
+            with open(version_path) as version_file:
+                version = json.load(version_file)
+        return version
+
+    def get_installed_version(self) -> str:
+        versions = self.get_installed()
+        if versions['installed']['name'] == self.channel:
+            ver = versions['installed']['sha']
+        else:
+            ver = None
+        return ver
+
+    def get_available_versions(self) -> list:
+        branches_path = os.path.join(self.db_path, "branches.json")
+        versions = []
+        if os.path.exists(branches_path):
+            with open(branches_path) as branches_file:
+                branches = json.load(branches_file)
+                for branch in branches:
+                    versions.append({
+                        "name": branch['name'],
+                        "sha": branch['commit']['sha']
+                                    })
+        return versions
+
+    def check_update(self) -> bool:
+        available_versions = self.get_available_versions()
+        installed_version = self.get_installed_version()
+        updated = False
+        for versions in available_versions:
+            if self.channel == versions['name']:
+                if (installed_version == versions['sha']):
+                    updated = True
+        return updated
+
+    def get_update_sha(self) -> str:
+        available_versions = self.get_available_versions()
+        sha = None
+        for versions in available_versions:
+            if self.channel == versions['name']:
+                sha = versions['sha']
+        return sha
+
+    def download_package(self, sha):
+        url = ('https://github.com/ChimeraOS/chimera-data/archive/' +
+               sha + '.zip')
+        resp = requests.get(url, timeout=1)
+        if resp.status_code == 200:
+            zip_path = os.path.join(self.db_path, "data.zip")
+            with open(zip_path, 'wb') as zip_file:
+                zip_file.write(resp.content)
+        else:
+            raise Exception(f"File not found for sha: {sha}")
+
+    def update(self, force=False) -> bool:
+        updated = self.check_update()
+        if not updated or force:
+            sha = self.get_update_sha()
+            self.download_package(sha)
+            zip_path = os.path.join(self.db_path, "data.zip")
+            version_path = os.path.join(self.db_path, "versions.json")
+            with zipfile.ZipFile(zip_path, 'r') as zip_file:
+                zip_file.extractall(self.db_path)
+            dirname = os.path.join(self.db_path, 'chimera-data-' + sha)
+            for data in os.listdir(dirname):
+                src_path = os.path.join(dirname, data)
+                dest_path = os.path.join(self.db_path, data)
+                if os.path.isdir(src_path):
+                    shutil.copytree(src_path,
+                                    dest_path,
+                                    dirs_exist_ok=True)
+                elif os.path.isfile(src_path):
+                    shutil.copyfile(src_path, dest_path)
+            shutil.rmtree(dirname)
+            with open(version_path, 'w') as version_file:
+                ver_content = {"installed":
+                               {
+                                   "name": self.channel,
+                                   "sha": sha
+                                }
+                               }
+                version_file.write(json.dumps(ver_content,
+                                              sort_keys=True,
+                                              indent=4)
+                                   )
+            updated = True
+        return updated

chimera_app/functions.py
@@ -1,134 +1,0 @@
-import re
-import os
-import shutil
-import yaml
-from PIL import Image, ImageFont, ImageDraw
-from chimera_app.utils import ensure_directory
-from chimera_app.utils import file_exists
-from chimera_app.utils import ChimeraContext
-
-
-def sanitize(string):
-    if isinstance(string, str):
-        retval = string
-        for r in ['\n', '\r', '/', '\\', '\0']:
-            retval = retval.replace(r, '_')
-        retval.replace('"', '')
-        return retval
-    return string
-
-
-def load_shortcuts(platform):
-    shortcuts = []
-    se = ChimeraContext()
-    ensure_directory(se.SHORTCUT_DIRS[0])
-
-    shortcuts_file = se.SHORTCUT_DIRS[0] + \
-        "/chimera.{platform}.yaml".format(platform=platform)
-    if file_exists(shortcuts_file):
-        shortcuts = yaml.load(open(shortcuts_file), Loader=yaml.Loader)
-
-    if not shortcuts:
-        shortcuts = []
-
-    return shortcuts
-
-
-def delete_file_link(base_dir, platform, name):
-    e = re.escape(name) + r"\.[^.]+$"
-    d = os.path.join(base_dir, platform)
-    links = []
-    if os.path.isdir(d):
-        links = [os.path.join(d, l) for l in os.listdir(d) if re.match(e, l)]
-
-    if len(links) < 1:
-        return
-
-    for link in links:
-        if os.path.islink(link) or os.path.exists(link):
-            os.remove(link)
-
-
-def is_direct(platform, content_type):
-    return (platform == "arcade" or platform == "neo-geo") and content_type == "content"
-
-
-def upsert_file(src_path, base_dir, platform, name, dst_name):
-    if not src_path:
-        return
-
-    content_type = os.path.basename(base_dir)
-    filename = sanitize(dst_name)
-    file_dir = "{base_dir}/{platform}/.{name}".format(base_dir=base_dir, platform=platform, name=name)
-
-    # mame ROM files have dependencies on each other, so store them all in a single directory
-    if is_direct(platform, content_type):
-        file_dir = "{base_dir}/{platform}/.{platform}".format(base_dir=base_dir, platform=platform)
-
-    if not os.path.exists(file_dir):
-        os.makedirs(file_dir)
-
-    file_path = "{file_dir}/{filename}".format(file_dir=file_dir, filename=filename)
-    if os.path.exists(file_path):
-        os.remove(file_path)
-
-    shutil.move(src_path, file_path)
-
-    _, ext = os.path.splitext(filename)
-    dst = "{base_dir}/{platform}/{name}{ext}".format(base_dir=base_dir, platform=platform, name=name, ext=ext)
-
-    delete_file_link(base_dir, platform, name)
-    os.symlink(file_path, dst)
-
-    # mame requires ROM files to have a specific name, so launch original file directly
-    if is_direct(platform, content_type):
-        return file_path
-
-    return dst
-
-
-def strip(string):
-    if string.startswith('"') and string.endswith('"'):
-        return string[1:-1]
-    return string
-
-
-def delete_file(base_dir, platform, name):
-    if is_direct(platform, os.path.basename(base_dir)):
-        shortcuts = load_shortcuts(platform)
-        matches = [e for e in shortcuts if e['name'] == name and e['cmd'] == platform]
-        shortcut = matches[0]
-        if 'dir' in shortcut and 'params' in shortcut:
-            file_path = os.path.join(strip(shortcut['dir']), strip(shortcut['params']))
-            if os.path.exists(file_path):
-                os.remove(file_path)
-    else:
-        file_dir = "{base_dir}/{platform}/.{name}".format(base_dir=base_dir, platform=platform, name=name)
-        if os.path.exists(file_dir):
-            shutil.rmtree(file_dir)
-
-    delete_file_link(base_dir, platform, name)
-
-
-def generate_banner(text, path):
-    # The thumbnail size used by Steam is set
-    banner_width = 460
-    banner_height = 215
-    banner = Image.new('RGB', (banner_width, banner_height), color=(0, 0, 0))
-
-    font = ImageFont.truetype("/usr/share/fonts/TTF/DejaVuSansMono-Bold.ttf", 24)
-
-    text_width, text_height = font.getsize(text)
-
-    # Shorten the text if it doesn't fit on the image
-    while text_width > banner_width:
-        text = text[:-4] + "..."
-        text_width, text_height = font.getsize(text)
-
-    text_x = int(banner_width / 2 - text_width / 2)
-    text_y = int(banner_height / 2 - text_height / 2)
-
-    title = ImageDraw.Draw(banner)
-    title.text((text_x, text_y), text, font=font, fill=(255, 255, 255))
-
-    banner.save(path)

chimera_app/mangohud_config.py
@@ -1,7 +1,6 @@
 import os
 from configparser import ConfigParser
 from chimera_app.utils import ensure_directory_for_file
-from chimera_app.utils import file_exists
 
 
 class MangoHudConfig:
@@ -12,7 +11,7 @@ class MangoHudConfig:
         self.__read_toggle_key()
 
     def __setup_environment(self):
-        if not file_exists(self.config_file):
+        if not os.path.exists(self.config_file):
             self.reset_config()
 
     def __read_toggle_key(self):
@@ -20,7 +19,7 @@ class MangoHudConfig:
         parser = ConfigParser(allow_no_value=True)
 
         # Check in file for override
-        if file_exists(self.config_file):
+        if os.path.exists(self.config_file):
             f = open(self.config_file, "r")
             parser.read_string("[Mangohud]\n" + f.read())
             f.close()
@@ -45,7 +44,7 @@ class MangoHudConfig:
         self.save_config("\n".join(defaultt_config))
 
     def get_current_config(self):
-        if file_exists(self.config_file):
+        if os.path.exists(self.config_file):
             f = open(self.config_file, "r")
             current_config = f.read()
             f.close()

commit 6b7bc9677807d659d5eaf4232d7744030410ea64

Fix some bugs introduced in #191 (#194)
* Fix typo in argument parser

* Fix SHORTCUT_DIRS usage on utils and config

chimera_app/__main__.py
@@ -15,11 +15,11 @@ def setup_argparse():
         description='Chimera app for managing ChimeraOS')
 
     parser.add_argument('-u', '--update',
-                        action="store-true",
+                        action="store_true",
                         help='Update data from repository'
                         )
     parser.add_argument('-f', '--force-update',
-                        action="store-true",
+                        action="store_true",
                         help='Force update even if already updated'
                         )
     group_ex = parser.add_mutually_exclusive_group()

chimera_app/config.py
@@ -17,7 +17,7 @@ AUTHENTICATOR_PATH = os.path.abspath('bin/chimera-authenticator')
 if not os.path.isfile(AUTHENTICATOR_PATH):
     AUTHENTICATOR_PATH = "/usr/share/chimera/bin/chimera-authenticator"
 
-SHORTCUT_DIR = context.SHORTCUT_DIRS[0]
+SHORTCUT_DIR = context.SHORTCUT_DIRS
 BANNER_DIR = context.DATA_HOME + '/chimera/banners'
 CONTENT_DIR = context.DATA_HOME + '/chimera/content'
 RECORDINGS_DIR = context.DATA_HOME + '/chimera/recordings'

chimera_app/utils.py
@@ -44,9 +44,9 @@ def sanitize(string):
 
 def load_shortcuts(platform):
     shortcuts = []
-    ensure_directory(context.SHORTCUT_DIRS[0])
+    ensure_directory(context.SHORTCUT_DIRS)
 
-    shortcuts_file = (context.SHORTCUT_DIRS[0] +
+    shortcuts_file = (context.SHORTCUT_DIRS +
                       "/chimera.{platform}.yaml".format(platform=platform))
     if os.path.exists(shortcuts_file):
         shortcuts = yaml.load(open(shortcuts_file), Loader=yaml.Loader)