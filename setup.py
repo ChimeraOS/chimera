a8465e45648d6f3fcff23bb3b82d5e30e46dc4ed
Alesh Slovak <aleshslovak@gmail.com>
2021-11-30 11:14:24 -0500

    bump version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="Chimera",
-    version="0.12.2",
+    version="0.12.5",
     packages=find_packages(exclude=['tests']),
     entry_points={
         'console_scripts': ['chimera = chimera_app.__main__:main']
