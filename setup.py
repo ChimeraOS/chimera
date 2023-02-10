cebc45f7dec755e8a35cba4675c8913ddf072436
Joaquín Ignacio Aramendía <samsagax@gmail.com>
2023-02-10 12:00:41 -0300

    Package animated splash

--- setup.py
+++ setup.py
@@ -17,6 +17,7 @@ setup(
         ('share/chimera/images', glob('images/*.png')),
         ('share/chimera/images/flathub', glob('images/flathub/*.png')),
         ('share/chimera/images/splash', glob('images/splash/*.png')),
+        ('share/chimera/images/splash', glob('images/splash/*.webm')),
         ('share/chimera/views', glob('views/*.tpl')),
         ('share/chimera/public', glob('public/*.js')),
         ('share/chimera/public', glob('public/*.css')),
