543703c85b98ade0546dcaa8d9821e04b206de75
Alesh Slovak <aleshslovak@gmail.com>
2024-01-09 08:56:32 -0500

    fix crashing splash screen

--- chimera_app/splash_screen.py
+++ chimera_app/splash_screen.py
@@ -12,12 +12,4 @@ class SplashScreen:
 
     def launch(self) -> None:
         print("Showing splash screen")
-        self.kill()
         subprocess.Popen([self.__fg, self.__app])
-
-    def kill(self):
-        for process in psutil.process_iter():
-            if self.__app in process.cmdline() and self.__fg not in process.cmdline():
-                print("Stopping splash screen")
-                process.kill()
-                break
